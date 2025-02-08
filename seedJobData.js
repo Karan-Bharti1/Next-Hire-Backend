84b66d918ad5ddf65bb70089f7a64c0525575801
Karan-Bharti1 <karan.codes1@gmail.com>
2025-02-08 11:25:04 +0530

    feat: seeded jobs data into the database

---
+++ seedJobData.js
@@ -0,0 +1,26 @@
+const {databaseInitialiser}=require('./database/databaseConnection')
+databaseInitialiser()
+const fs=require('fs')
+const Jobs=require('./models/Jobsmodels')
+const jsonData=fs.readFileSync("jobs.json",'utf-8')
+const jobsData=JSON.parse(jsonData)
+async function seedJobsData(){
+    try {
+        for(const jobData of jobsData){
+            const newJob=new Jobs({
+                title:jobData.title,
+                companyName: jobData.companyName,
+        location: jobData.location,
+        salary: jobData.salary,
+        type: jobData.type,
+        description: jobData.description,
+        qualifications: jobData.qualifications
+            })
+            await newJob.save()
+        }
+        
+    } catch (error) {
+        console.log("Error while seeding the data",error) 
+    }
+}
+// seedJobsData()
